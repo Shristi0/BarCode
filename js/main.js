5f6fc4531ecf3dba78825f3d560ef885b4297f5a
Shristi0 <sristyshrestha93@gmail.com>
2024-08-12 18:21:27 +0545

    home page completed almost

--- js/main.js
+++ js/main.js
@@ -1,34 +1,3 @@
-/*--------------------------------------------------
-
-
-NOTE: main.js, All custom script and plugin activation script in this file. 
------------------------------------------------------
-
-    JS INDEX
-    ================================================
-    1. Newsletter Popup
-    2. Mobile Menu Activation
-    3. Tooltip Activation
-    4. Cart Box Dropdown Menu 
-    5. Checkout Page Activation
-    6. Slider Activation
-    7. Thumbnail Product activation
-    8. Apple Watch Product activation
-    9. Recent Post Activation
-    10. Featured Product Activation
-    11. Categorie Product Activation
-    12. Deal Product Activation
-    13. Onsale Product Activation
-    14. Countdown Js Activation
-    15. ScrollUp Activation
-    16. Sticky-Menu Activation
-    17. Nice Select Activation
-    18. Price Slider Activation
-    19. Brand Logo  Activation
-    
-
-================================================*/
-
 (function ($) {
     "use Strict";
 
@@ -394,5 +363,72 @@ NOTE: main.js, All custom script and plugin activation script in this file.
             }
         }
     })
+
+    /*--------------------------------
+    20. Counter 
+    ---------------------------------*/
+    function inVisible(element) {
+        //Checking if the element is
+        //visible in the viewport
+        var WindowTop = $(window).scrollTop();
+        var WindowBottom = WindowTop + $(window).height();
+        var ElementTop = element.offset().top;
+        var ElementBottom = ElementTop + element.height();
+        //animating the element if it is
+        //visible in the viewport
+        if ((ElementBottom <= WindowBottom) && ElementTop >= WindowTop)
+            animate(element);
+    }
+
+    $(document).ready(function () {
+        $("#news-slider").owlCarousel({
+            items: 3,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [980, 2],
+            itemsMobile: [600, 1],
+            navigation: true,
+            navigationText: ["", ""],
+            pagination: true,
+            autoPlay: true
+        });
+    });
+
+    function animate(element) {
+        //Animating the element if not animated before
+        if (!element.hasClass('ms-animated')) {
+            var maxval = element.data('max');
+            var html = element.html();
+            element.addClass("ms-animated");
+            $({
+                countNum: element.html()
+            }).animate({
+                countNum: maxval
+            }, {
+                //duration 5 seconds
+                duration: 5000,
+                easing: 'linear',
+                step: function () {
+                    element.html(Math.floor(this.countNum) + html);
+                },
+                complete: function () {
+                    element.html(this.countNum + html);
+                }
+            });
+        }
+
+    }
+
+    //When the document is ready
+    $(function () {
+        //This is triggered when the
+        //user scrolls the page
+        $(window).scroll(function () {
+            //Checking if each items to animate are 
+            //visible in the viewport
+            $("h1[data-max]").each(function () {
+                inVisible($(this));
+            });
+        })
+    });
 
 })(jQuery);
