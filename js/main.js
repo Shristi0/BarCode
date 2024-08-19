2e137d3927a2e630d605fbed25230d63648affe2
Shristi0 <sristyshrestha93@gmail.com>
2024-08-20 00:55:45 +0545

    almost done

--- js/main.js
+++ js/main.js
@@ -67,15 +67,26 @@
         loop: true,
         margin: 0,
         nav: true,
-        autoplay: false,
+        autoplay: true,
         animateOut: 'fadeOut',
         animateIn: 'fadeIn',
         items: 1,
         autoplayTimeout: 10000,
         navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-        dots: true,
+        
         autoHeight: true,
         lazyLoad: true,
+        responsive: {
+            0: {
+                dots:false,
+                
+                
+            },
+            520: {
+                dots: true,
+               
+            },
+        }
     });
 
     /*-------------------------------------
@@ -364,6 +375,26 @@
         }
     })
 
+    $('.blogs__slider').owlCarousel({
+        loop: true,
+        margin: 32,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+
     /*--------------------------------
     20. Counter 
     ---------------------------------*/
@@ -379,19 +410,6 @@
         if ((ElementBottom <= WindowBottom) && ElementTop >= WindowTop)
             animate(element);
     }
-
-    $(document).ready(function () {
-        $("#news-slider").owlCarousel({
-            items: 3,
-            itemsDesktop: [1199, 3],
-            itemsDesktopSmall: [980, 2],
-            itemsMobile: [600, 1],
-            navigation: true,
-            navigationText: ["", ""],
-            pagination: true,
-            autoPlay: true
-        });
-    });
 
     function animate(element) {
         //Animating the element if not animated before
@@ -430,5 +448,42 @@
             });
         })
     });
+
+    // 21. 
+    $("#carousel").owlCarousel({
+        autoplay: true,
+        rewind: false, /* use rewind if you don't want loop */
+        margin: 20,
+        loop: true,
+         /*
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        */
+        responsiveClass: true,
+        autoHeight: true,
+        autoplayTimeout: 7000,
+        smartSpeed: 800,
+        nav: true,
+        navText: [
+          '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>',
+          '<svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>'],
+        responsive: {
+          0: {
+            items: 1
+          },
+      
+          600: {
+            items: 3
+          },
+      
+          1024: {
+            items: 4
+          },
+      
+          1366: {
+            items: 4
+          }
+        }
+      });
 
 })(jQuery);
